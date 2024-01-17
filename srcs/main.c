6f52b3362da5a0fed6036d84b5c0f5c00793cc8c
Ugolin Ollé <hello@ugolin-olle.com>
2024-01-17 14:52:14 +0700

    docs: adding docstring

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,13 @@
 
 #include "../includes/fractol.h"
 
+/**
+ * @brief Check if the arguments are valid.
+ *
+ * @param argc int Number of arguments.
+ * @param argv char** Array of arguments.
+ * @param fract t_mlx Fractal struct.
+ */
 static void ft_check_args(int argc, char **argv, t_mlx *fract) {
   if (argc != 2)
     ft_handle_error("[ERROR] - Usage: ./fractol [fractal name]");
@@ -25,6 +32,11 @@ static void ft_check_args(int argc, char **argv, t_mlx *fract) {
     ft_handle_error("[ERROR] - Usage: ./fractol [fractal name]");
 }
 
+/**
+ * @brief Select the fractal to be drawn.
+ *
+ * @param fract t_mlx Fractal struct.
+ */
 static void ft_menu(t_mlx *fract) {
   if (ft_strcmp(fract->title, "Mandelbrot") == 0)
     ft_putstr_fd("[INFO] - Mandelbrot asn't been implemented yet.",
@@ -36,6 +48,13 @@ static void ft_menu(t_mlx *fract) {
                  STDOUT_FILENO);
 }
 
+/**
+ * @brief Main function.
+ *
+ * @param argc int Number of arguments.
+ * @param argv char** Array of arguments.
+ * @return int 0 if success.
+ */
 int main(int argc, char **argv) {
   t_mlx fract;
 
