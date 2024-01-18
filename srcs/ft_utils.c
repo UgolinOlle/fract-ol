dbdeb5c81cab55fd54b6926140fdafc8769f16b3
Ugolin Ollé <hello@ugolin-olle.com>
2024-01-18 13:54:56 +0700

    fix: update utils

--- srcs/ft_utils.c
+++ srcs/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: uolle <uolle@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 01:13:23 by uolle             #+#    #+#             */
-/*   Updated: 2024/01/16 23:52:08 by uolle            ###   ########.fr       */
+/*   Updated: 2024/01/17 23:31:11 by uolle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
  * @return int Number of iterations.
  */
 void ft_draw_pixel(t_mlx *fract, int x, int y, int color) {
-  if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+  if (fract->x < WIDTH && fract->y < HEIGHT) {
     color = mlx_get_color_value(fract->mlx, color);
     ft_memcpy(fract->data + 4 * WIDTH * y + x * 4, &color, sizeof(int));
   }
@@ -33,7 +33,7 @@ void ft_draw_pixel(t_mlx *fract, int x, int y, int color) {
  * @param error char* Error message.
  * @param type int Type of error.
  */
-void ft_exit(t_mlx *fract, int type) {
+int ft_exit(t_mlx *fract, int type) {
   if (type == 0) {
     mlx_destroy_image(fract->mlx, fract->img);
     mlx_destroy_window(fract->mlx, fract->win);
@@ -48,45 +48,5 @@ void ft_exit(t_mlx *fract, int type) {
       free(fract->mlx);
   }
   exit(EXIT_SUCCESS);
-}
-
-/**
- * @brief Print all element in the fractal struct.
- *
- * @param fract t_mlx Fractal struct.
- */
-void ft_print_mlx(t_mlx *fract) {
-  ft_putstr_fd("mlx: ", STDOUT_FILENO);
-  ft_putendl_fd(fract->mlx, STDOUT_FILENO);
-  ft_putstr_fd("win: ", STDOUT_FILENO);
-  ft_putendl_fd(fract->win, STDOUT_FILENO);
-  ft_putstr_fd("img: ", STDOUT_FILENO);
-  ft_putendl_fd(fract->img, STDOUT_FILENO);
-  ft_putstr_fd("data: ", STDOUT_FILENO);
-  ft_putendl_fd(fract->data, STDOUT_FILENO);
-  ft_putstr_fd("bpp: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->bpp, STDOUT_FILENO);
-  ft_putstr_fd("\nsize_line: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->size_line, STDOUT_FILENO);
-  ft_putstr_fd("\nendian: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->endian, STDOUT_FILENO);
-  ft_putstr_fd("\nx: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->x, STDOUT_FILENO);
-  ft_putstr_fd("\ny: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->y, STDOUT_FILENO);
-  ft_putstr_fd("\nx1: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->x1, STDOUT_FILENO);
-  ft_putstr_fd("\ny1: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->y1, STDOUT_FILENO);
-  ft_putstr_fd("\nc_r: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->c_r, STDOUT_FILENO);
-  ft_putstr_fd("\nc_i: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->c_i, STDOUT_FILENO);
-  ft_putstr_fd("\nmax_iter: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->max_iter, STDOUT_FILENO);
-  ft_putstr_fd("\nzoom: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->zoom, STDOUT_FILENO);
-  ft_putstr_fd("\ncolor: ", STDOUT_FILENO);
-  ft_putnbr_fd(fract->color, STDOUT_FILENO);
-  ft_putstr_fd("\n", STDOUT_FILENO);
+  return (0);
 }
