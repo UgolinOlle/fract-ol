85725d96b59ffebe2c2b559495b364cd5a7be9d6
Ugolin Ollé <hello@ugolin-olle.com>
2024-01-23 16:19:18 +0700

    fix: parsing error while no parameter

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: uolle <uolle@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 22:47:19 by uolle             #+#    #+#             */
-/*   Updated: 2024/01/23 15:03:04 by uolle            ###   ########.fr       */
+/*   Updated: 2024/01/23 16:17:31 by uolle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@
  */
 static void ft_check_args(int argc, char **argv, t_mlx *fract) {
   if (argc < 2)
-    ft_exit(fract, "[ERROR] - Usage: ./fractol [fractal name]");
+    ft_handle_error("[ERROR] - Usage: ./fractol [fractal name]");
   if (ft_strcmp(argv[1], "mandelbrot") == 0) {
     fract->title = "Mandelbrot";
     ft_init_mandelbrot(fract);
-  } else if (ft_strcmp(argv[1], "julia") == 0 && argv[2]) {
+  } else if (ft_strcmp(argv[1], "julia") == 0) {
+    if (argc < 3)
+      ft_handle_error("[ERROR] - Usage: ./fractol julia [1 or 2 or 3]");
     fract->title = "Julia";
     if (ft_atoi(argv[2]) == 1)
       ft_init_julia(fract, 1);
@@ -33,10 +35,16 @@ static void ft_check_args(int argc, char **argv, t_mlx *fract) {
       ft_init_julia(fract, 2);
     else if (ft_atoi(argv[2]) == 3)
       ft_init_julia(fract, 3);
+    else if (ft_atoi(argv[2]) == 0)
+      ft_handle_error("[ERROR] - Usage: ./fractol julia [1 or 2 or 3]");
     else
-      ft_exit(fract, "[ERROR] - Usage: ./fractol julia [1 or 2]");
-  } else
-    ft_exit(fract, "[ERROR] - Usage: ./fractol julia [real] [imaginary]");
+      ft_handle_error("[ERROR] - Usage: ./fractol julia [1 or 2 or 3]");
+  } else if (ft_strcmp(argv[1], "tricorn") == 0) {
+    fract->title = "Tricorn";
+    ft_init_tricorn(fract);
+  } else {
+    ft_handle_error("[ERROR] - Usage: ./fractol [fractal name]");
+  }
 }
 
 /**
@@ -50,6 +58,8 @@ int ft_exec_fract(t_mlx *fract) {
     ft_mandelbrot(fract);
   else if (!ft_strcmp(fract->title, "Julia"))
     ft_julia(fract);
+  else if (!ft_strcmp(fract->title, "Tricorn"))
+    ft_tricorn(fract);
   return (0);
 }
 
